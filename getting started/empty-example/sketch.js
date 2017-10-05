669785d47e9466b71f683ec81391d89a6450abf7
xing liu <31227004+cathrinexing@users.noreply.github.com>
2017-10-05 18:24:45 -0400

    4

--- getting started/empty-example/sketch.js
+++ getting started/empty-example/sketch.js
@@ -1,9 +1,20 @@
+
+
+
+
 function setup(){
     createCanvas(500,500);
     background(125);
     background(255,0,0);
     background("purple");
     background("#999999");
+    
+    /* 
+    
+    
+    
+    
+    */
 
 
 }

0e7b625b278b63fe6d0f715ef190b6f27676cafb
xing liu <31227004+cathrinexing@users.noreply.github.com>
2017-10-06 13:43:21 -0400

    3

--- homework-art/empty-example/sketch.js
+++ homework-art/empty-example/sketch.js
@@ -2,9 +2,8 @@ var posX = 0;
 var posY = 0;
 
 function setup() {
-    createCanvas(1000, 1000);
-    background(125); 
-    background("purple");
+    createCanvas(1000, 1000); 
+    background("beige");
   
 
    posX = width/2;
@@ -14,25 +13,37 @@ function setup() {
 }
 
 function draw() {
-    //noStroke(); //removes strokes from everything
+
     stroke(0);
     strokeWeight(1);
+    fill("#F9E397");
+    ellipse(posX, posY, 300, 300);
     fill("pink");
-    ellipse(posX, posY, 100, 100);
-    fill("red");
-    rect(posX -25, posY + 20, 50, 20);
+    rect(posX -40, posY + 90, 80, 20);
     fill(255);
-    ellipse(posX-10,posY-20, 10, 40);
-    ellipse(posX+10,posY-20, 10, 40);
-    fill(0,255,0);
-    ellipse(posX-10,posY-20, 10, 20);
-    ellipse(posX+10,posY-20, 10, 10);
-    stroke("cyan");
-    strokeWeight(5);
-    point(posX + 10,posY + 10);
-    stroke(125,0,0);
-    line(posX - 25, posY + 30, posX + 25, posY + 30);
-    noFill();
-    arc(posX,posY,100,100, PI, TWO_PI);
+    ellipse(posX-60,posY-40, 50, 60);
+    ellipse(posX+60,posY-40, 50, 60);
+    fill(0);
+    ellipse(posX-60,posY-40, 20, 20);
+    ellipse(posX+60,posY-40, 20, 20);   
+    fill("yellow");
+    triangle(posX,posY - 10,posX-30,posY + 65,posX+30,posY +65);
+     
+    stroke(0);
+    line(posX - 40, posY+ 110, posX + 40, posY + 90);
+    line(posX - 40, posY+ 90, posX + 40, posY + 110);
+
+     noFill();
+    arc(posX,posY,175,300, PI+QUARTER_PI, TWO_PI-QUARTER_PI);
+    arc(posX,posY,200,300, PI, TWO_PI);
+    arc(posX,posY,225,300, PI, TWO_PI);
+    arc(posX,posY,250,300, PI, TWO_PI);
+    arc(posX,posY,275,300, PI, TWO_PI);
+    
+    stroke("#937E57");
+    strokeWeight(3);
+    point(posX + 80,posY + 10);
+    point(posX - 80,posY + 10);
+    point(posX + 10,posY + 120);
 
 }

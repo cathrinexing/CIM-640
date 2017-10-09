4325743df84291b924b79db4d78ca647eb43312a
xing liu <31227004+cathrinexing@users.noreply.github.com>
2017-10-09 14:27:58 -0400

    4

--- homework-art/empty-example/sketch.js
+++ homework-art/empty-example/sketch.js
@@ -3,7 +3,7 @@ var posY = 0;
 
 function setup() {
     createCanvas(1000, 1000); 
-    background("beige");
+    background("grey");
   
 
    posX = width/2;
@@ -16,35 +16,57 @@ function draw() {
 
     stroke(0);
     strokeWeight(1);
-    fill("#F9E397");
-    ellipse(posX, posY, 300, 300);
-    fill("pink");
-    rect(posX -40, posY + 90, 80, 20);
+    
+     noStroke();
+    
+    
+    fill("beige");
+    ellipse(posX, posY, 250, 300);
+   
     fill(255);
-    ellipse(posX-60,posY-40, 50, 60);
-    ellipse(posX+60,posY-40, 50, 60);
+    ellipse(posX-60,posY-30, 80,90);
+    ellipse(posX+60,posY-30, 80, 90);
     fill(0);
-    ellipse(posX-60,posY-40, 20, 20);
-    ellipse(posX+60,posY-40, 20, 20);   
+    ellipse(posX-60,posY-40, 30, 40);
+    ellipse(posX+60,posY-40, 30, 40);   
     fill("yellow");
-    triangle(posX,posY - 10,posX-30,posY + 65,posX+30,posY +65);
+    triangle(posX,posY + 10,posX-20,posY + 65,posX+20,posY +65);
+    
      
-    stroke(0);
-    line(posX - 40, posY+ 110, posX + 40, posY + 90);
-    line(posX - 40, posY+ 90, posX + 40, posY + 110);
-
-     noFill();
+     stroke(0);
+    noFill();
     arc(posX,posY,175,300, PI+QUARTER_PI, TWO_PI-QUARTER_PI);
     arc(posX,posY,200,300, PI, TWO_PI);
     arc(posX,posY,225,300, PI, TWO_PI);
     arc(posX,posY,250,300, PI, TWO_PI);
     arc(posX,posY,275,300, PI, TWO_PI);
+      
+      arc(posX-10,posY+100,25,25, 0, PI);
+     arc(posX+10,posY+100,25,25, 0, PI);
+     
+    fill("pink");
+    noStroke();
+ ellipse(posX,posY+100, 30, 20); 
     
     stroke("#937E57");
     strokeWeight(5);
     point(posX + 80,posY + 10);
-    strokeWeight(3);
-    point(posX - 80,posY + 10);
-    point(posX + 10,posY + 120);
+    strokeWeight(8);
+    point(posX,posY + 100);
+    
+    noStroke();
+     fill("beige");
+translate(width/2, height/2);
+rotate(PI/3.0);
+ellipse(-200, 20, 200, 70);
+      fill("pink");
+    ellipse(-200, 20, 100, 35);
+    rotate(PI/3.0);
+     fill("beige");
+    ellipse(-200, -20, 200, 70);
+      fill("pink");
+      ellipse(-200, -20, 100, 35);
+    
+     
 
 }
